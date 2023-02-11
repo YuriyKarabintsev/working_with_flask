cec9ef7b3fe7c527bb6ec876592c3d31e9a4c745
YuriyKarabintsev <urakarabintsev@gmail.com>
2023-02-11 16:58:34 +0700

    Task8 - now you can load a photo

--- main.py
+++ main.py
@@ -210,60 +210,64 @@ def show_results(nickname, level, rating):
 
 
 @app.route('/load_photo', methods=['POST', 'GET'])
-def sample_file_upload():
-    print(request.method, "- МЕТОД ЗАПРОСА К СТРАНИЦЕ С КАРТИНКОЙ")
+def load_photo():
+    file = url_for('static', filename='img/photo.jpg')
     if request.method == 'GET':
         return f'''<!doctype html>
                         <html lang="en">
                           <head>
                             <meta charset="utf-8">
                             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                             <link rel="stylesheet"
-                             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                             integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                             crossorigin="anonymous">
+                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
                             <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                            <title>Пример загрузки файла</title>
+                            <title>Отбор астронавтов</title>
                           </head>
                           <body>
-                            <h1>Загрузим файл</h1>
-                            <form method="post" enctype="multipart/form-data">
-                               <div class="form-group">
-                                    <label for="photo">Выберите файл</label>
-                                    <input type="file" class="form-control-file" id="photo" name="file">
-                                </div>
-                                <button type="submit" class="btn btn-primary">Отправить</button>
-                            </form>
+                            <h1 align="center">Загрузка фотографии</h1>
+                            <h3 align="center">для участия в миссии</h3>
+                            <div>
+                                <form class="login_form" method="post" enctype="multipart/form-data" style="background-color: #ffb3c2">
+                                   <div class="form-group">
+                                        <label for="photo"><u>Приложите фотографию</u></label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <img src="{file}" alt="Фото">
+                                    <br>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                            </div>
                           </body>
                         </html>'''
     elif request.method == 'POST':
         f = request.files['file']
-        print(f.read())
-        #f.save()
-        f'''<!doctype html>
-                                <html lang="en">
-                                  <head>
-                                    <meta charset="utf-8">
-                                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-                                     <link rel="stylesheet"
-                                     href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-                                     integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-                                     crossorigin="anonymous">
-                                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
-                                    <title>Пример загрузки файла</title>
-                                  </head>
-                                  <body>
-                                    <h1>Загрузим файл</h1>
-                                    <form method="post" enctype="multipart/form-data">
-                                       <div class="form-group">
-                                            <label for="photo">Выберите файл</label>
-                                            <img src="{f}" />
-                                            <input type="file" class="form-control-file" id="photo" name="file">
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Отправить</button>
-                                    </form>
-                                  </body>
-                                </html>'''
+        with open(f'static/img/{f.filename}', 'wb') as file:
+            file.write(f.read())
+        file = url_for('static', filename=f'img/{f.filename}')
+        return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+                            <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                            <title>Отбор астронавтов</title>
+                          </head>
+                          <body>
+                            <h1 align="center" color="darkgreen">Загрузка фотографии</h1>
+                            <h3 align="center" color="darkgreen">для участия в миссии</h3>
+                            <div>
+                                <form class="login_form" method="post" enctype="multipart/form-data" style="background-color: #ffb3c2">
+                                   <div class="form-group">
+                                        <label for="photo"><u>Приложите фотографию</u></label>
+                                        <input type="file" class="form-control-file" id="photo" name="file">
+                                    </div>
+                                    <img src="{file}" alt="Фото">
+                                    <br>
+                                    <button type="submit" class="btn btn-primary">Отправить</button>
+                                </form>
+                            </div>
+                          </body>
+                        </html>'''
 
 
 if __name__ == '__main__':
